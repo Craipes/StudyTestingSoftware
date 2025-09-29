623717ecc5581de1acd4a9992e6cb19e003fbdc0
Yurchuk Roman Volodymyrovych <craipes26@gmail.com>
2025-09-29 17:26:54 +0300

    Implemented AResult for TeacherGroupsController

--- StudyTestingSoftware/ProblemsDefinitions/GeneralErrors.cs
+++ StudyTestingSoftware/ProblemsDefinitions/GeneralErrors.cs
@@ -5,4 +5,6 @@ public static class GeneralErrors
     public const string InvalidInput = "G_InvalidInput";
     public const string UnauthorizedAccess = "G_UnauthorizedAccess";
     public const string ResourceNotFound = "G_ResourceNotFound";
+    public const string IdRequired = "G_IdRequired";
+    public const string EmailRequired = "G_EmailRequired";
 }
